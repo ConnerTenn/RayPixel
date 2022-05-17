d8c196abc808488ebdfd66034b55a690d05e1fc6
Conner <connertenn@gmail.com>
2022-05-17 11:28:02 -0400

    Added triangle list rendering

--- RayTrace.go
+++ RayTrace.go
@@ -74,13 +74,23 @@ func RayDir(fov float64, x int, y int, width int, height int) *v3.Vector3 {
 func RayTrace(ray Ray) {
 }
 
+var t float64
+
 func Render(tex *Texture) {
 
-	tri := Triangle{
-		P1: &v3.Vector3{X: -1, Y: 3, Z: 0},
-		P2: &v3.Vector3{X: 1, Y: 3, Z: 0},
-		P3: &v3.Vector3{X: 0, Y: 3, Z: 1},
+	triangles := []Triangle{
+		Triangle{
+			P1: &v3.Vector3{X: -1, Y: 3, Z: 0},
+			P2: &v3.Vector3{X: 1, Y: 3, Z: 0},
+			P3: &v3.Vector3{X: 0, Y: 3, Z: 1},
+		},
+		Triangle{
+			P1: &v3.Vector3{X: 0, Y: 5, Z: 1},
+			P2: &v3.Vector3{X: 2, Y: 5, Z: 1},
+			P3: &v3.Vector3{X: 1, Y: 5, Z: 2},
+		},
 	}
+	t += 3.1415 / 60.0
 
 	wait := sync.WaitGroup{}
 	for y := 0; y < tex.Height; y++ {
@@ -88,13 +98,20 @@ func Render(tex *Texture) {
 		go func(y int) {
 			for x := 0; x < tex.Width; x++ {
 				ray := Ray{
-					Pos: &v3.Vector3{X: 0, Y: 0, Z: 0},
+					Pos: &v3.Vector3{X: math.Sin(t), Y: 0, Z: 0},
 					Dir: RayDir(50, x, y, tex.Width, tex.Height),
 				}
 
-				intersect, _, _ := ray.Intersect(&tri)
+				var intersected *Triangle = nil
+				for _, tri := range triangles {
+					intersect, _, _ := ray.Intersect(&tri)
 
-				if intersect {
+					if intersect {
+						intersected = &tri
+					}
+				}
+
+				if intersected != nil {
 					tex.Set(x, y, Pixel{
 						Red:   byte(255 * (x + y) / (tex.Width + tex.Height)),
 						Green: byte(255 * x / tex.Width),
