638f4630f2a6fc125494bcae62e34e0734d82aee
Conner <connertenn@gmail.com>
2022-05-19 03:34:54 -0400

    Improved render scene

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 	model.Rotate(stl.Vec3{0, 0, 0}, stl.Vec3{1, 0, 0}, Tau/4)
 	model.Rotate(stl.Vec3{0, 0, 0}, stl.Vec3{0, 0, 1}, Tau/8)
 	min := model.Measure().Min
-	model.Translate(stl.Vec3{-2, 10, -min[2]})
+	model.Translate(stl.Vec3{-1.5, 10, -min[2]})
 
 	triangles := make([]Triangle, 1)
 	for _, tri := range model.Triangles {
@@ -36,7 +36,7 @@ func main() {
 				NewVec3(float64(tri.Vertices[1][0]), float64(tri.Vertices[1][1]), float64(tri.Vertices[1][2])),
 				NewVec3(float64(tri.Vertices[2][0]), float64(tri.Vertices[2][1]), float64(tri.Vertices[2][2])),
 				Material{
-					SurfaceColour: NewColour(0.1, 0.5, 0.5),
+					SurfaceColour: NewColour(1, 0.8, 0.5),
 					Diffuse:       1.0,
 					Metallic:      0.2,
 					Emissive:      0,
@@ -77,7 +77,18 @@ func main() {
 				SurfaceColour: NewColour(1, 0.2, 1),
 				Diffuse:       0,
 				Metallic:      0,
-				Emissive:      20,
+				Emissive:      5,
+			},
+		),
+		NewTriangle(
+			NewVec3(-8, 0, 0),
+			NewVec3(-8, 20, 0),
+			NewVec3(-8, 0, 10),
+			Material{
+				SurfaceColour: NewColour(0.2, 0.2, 1),
+				Diffuse:       0,
+				Metallic:      0,
+				Emissive:      5,
 			},
 		),
 	}...)
@@ -122,7 +133,7 @@ func main() {
 
 		select {
 		case <-ticker.C:
-			fmt.Print("\rFrameRate:", int(1.0/dt.Seconds()), "  FrameTime:", int(dt.Milliseconds()), "ms      ")
+			fmt.Print("\rFrameRate:", int(1.0/dt.Seconds()), "  FrameTime:", int(dt.Milliseconds()), "ms", "  Samples:", NumSamples, "      ")
 		default:
 		}
 	}
